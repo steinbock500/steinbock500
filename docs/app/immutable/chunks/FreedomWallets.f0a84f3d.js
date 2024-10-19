d39ec9acb68476de29ac369bfba7a54b8c85a564
Steinbock500 <michael@steinbock500.ch>
2024-10-19 13:47:27 +0200

    free the world

---
+++ docs/app/immutable/chunks/FreedomWallets.f0a84f3d.js
@@ -0,0 +1,45 @@
+import{s as ke,f as Le,n as $}from"./scheduler.63274e7e.js";import{S as Te,i as Pe,g as l,s as m,m as a,h as r,z as s,c as x,n as h,k as u,a as n,f as i}from"./index.9e07f724.js";function oe(He){let p,ee="<br/><br/><br/>",G,_,te="Schnellkurs Online Geldbeutel",O,w,ne="<br/>",q,C,ie="<br/>",W,v,le="metamask.io",j,b,re="brave.com",I,H,se="<br/>",y,k,ue="<br/>",A,L,ae="<br/>",E,T,he="<br/><br/>",g,P,fe="Schnellkurs Offline Geldbeutel",K,o,pe="<br/>",V,z,ve="<br/>",J,f,be="Privatsphäre ist kein Verbrechen",N,c,de="<br/>",Q,S,me="<br/>",R,d,xe="hier",U,B,Me="<br/>",X,D,_e="<br/>",Y,M,we,Z,F,Ce="<br/>";return{c(){p=l("p"),p.innerHTML=ee,G=m(),_=l("h6"),_.textContent=te,O=m(),w=l("p"),w.innerHTML=ne,q=a(`
+
+So wie wir einen Browser nutzen, um im Internet zu surfen, nutzen wir Online Wallets, um mit
+Blockchains zu interagieren.
+`),C=l("p"),C.innerHTML=ie,W=a(`
+Für kleine Geldmengen, sowie für die Interaktion mit Blockchains nutzen wir Online Wallets wie z.B.
+`),v=l("a"),v.textContent=le,j=a(`
+oder das im `),b=l("a"),b.textContent=re,I=a(` Browser eingebaute Wallet.
+`),H=l("p"),H.innerHTML=se,y=a(`
+Hier kann es das Problem geben, dass ausgediente totalitäre Strukturen mitten in sogenannten Demokratien die Überwachung friedliebender und freiheitsliebender Menschen übertreiben.
+`),k=l("p"),k.innerHTML=ue,A=a(`
+Daher nutzen wir für die Kooperation von Mensch zu Mensch ohne übertriebene Überwachung 
+im Hintergrund eher Offline Geldbeutel. `),L=l("p"),L.innerHTML=ae,E=m(),T=l("p"),T.innerHTML=he,g=m(),P=l("h6"),P.textContent=fe,K=m(),o=l("p"),o.innerHTML=pe,V=a(`
+Eine der besten Möglichkeiten Freiheitsgeld aufzubewahren sind self hosted (nur Du hast den Privat Schlüssel),
+cold (nicht mit dem Internet verbundene) Paperwallets.
+`),z=l("p"),z.innerHTML=ve,J=m(),f=l("a"),f.textContent=be,N=a(`. 
+`),c=l("p"),c.innerHTML=de,Q=a(`
+Sie scheint eher einer der Garanten für Freiheit, Weltfrieden und Freundschaft zu sein. 
+`),S=l("p"),S.innerHTML=me,R=a(`
+
+Den Open Source Code unseres Paperwallet Generators findest Du
+`),d=l("a"),d.textContent=xe,U=a(`.
+`),B=l("p"),B.innerHTML=Me,X=m(),D=l("p"),D.innerHTML=_e,Y=m(),M=l("embed"),Z=m(),F=l("p"),F.innerHTML=Ce,this.h()},l(e){p=r(e,"P",{"data-svelte-h":!0}),s(p)!=="svelte-ednd6n"&&(p.innerHTML=ee),G=x(e),_=r(e,"H6",{"data-svelte-h":!0}),s(_)!=="svelte-1och1ea"&&(_.textContent=te),O=x(e),w=r(e,"P",{"data-svelte-h":!0}),s(w)!=="svelte-pntvnr"&&(w.innerHTML=ne),q=h(e,`
+
+So wie wir einen Browser nutzen, um im Internet zu surfen, nutzen wir Online Wallets, um mit
+Blockchains zu interagieren.
+`),C=r(e,"P",{"data-svelte-h":!0}),s(C)!=="svelte-pntvnr"&&(C.innerHTML=ie),W=h(e,`
+Für kleine Geldmengen, sowie für die Interaktion mit Blockchains nutzen wir Online Wallets wie z.B.
+`),v=r(e,"A",{href:!0,target:!0,"data-svelte-h":!0}),s(v)!=="svelte-ckwul2"&&(v.textContent=le),j=h(e,`
+oder das im `),b=r(e,"A",{href:!0,target:!0,"data-svelte-h":!0}),s(b)!=="svelte-12y5jy6"&&(b.textContent=re),I=h(e,` Browser eingebaute Wallet.
+`),H=r(e,"P",{"data-svelte-h":!0}),s(H)!=="svelte-pntvnr"&&(H.innerHTML=se),y=h(e,`
+Hier kann es das Problem geben, dass ausgediente totalitäre Strukturen mitten in sogenannten Demokratien die Überwachung friedliebender und freiheitsliebender Menschen übertreiben.
+`),k=r(e,"P",{"data-svelte-h":!0}),s(k)!=="svelte-jq895e"&&(k.innerHTML=ue),A=h(e,`
+Daher nutzen wir für die Kooperation von Mensch zu Mensch ohne übertriebene Überwachung 
+im Hintergrund eher Offline Geldbeutel. `),L=r(e,"P",{"data-svelte-h":!0}),s(L)!=="svelte-jq895e"&&(L.innerHTML=ae),E=x(e),T=r(e,"P",{"data-svelte-h":!0}),s(T)!=="svelte-5q9igk"&&(T.innerHTML=he),g=x(e),P=r(e,"H6",{"data-svelte-h":!0}),s(P)!=="svelte-1fzr742"&&(P.textContent=fe),K=x(e),o=r(e,"P",{"data-svelte-h":!0}),s(o)!=="svelte-pntvnr"&&(o.innerHTML=pe),V=h(e,`
+Eine der besten Möglichkeiten Freiheitsgeld aufzubewahren sind self hosted (nur Du hast den Privat Schlüssel),
+cold (nicht mit dem Internet verbundene) Paperwallets.
+`),z=r(e,"P",{"data-svelte-h":!0}),s(z)!=="svelte-pntvnr"&&(z.innerHTML=ve),J=x(e),f=r(e,"A",{href:!0,target:!0,"data-svelte-h":!0,"data-svelte-h":!0}),s(f)!=="svelte-twqne9"&&(f.textContent=be),N=h(e,`. 
+`),c=r(e,"P",{"data-svelte-h":!0}),s(c)!=="svelte-jq895e"&&(c.innerHTML=de),Q=h(e,`
+Sie scheint eher einer der Garanten für Freiheit, Weltfrieden und Freundschaft zu sein. 
+`),S=r(e,"P",{"data-svelte-h":!0}),s(S)!=="svelte-jq895e"&&(S.innerHTML=me),R=h(e,`
+
+Den Open Source Code unseres Paperwallet Generators findest Du
+`),d=r(e,"A",{href:!0,target:!0,"data-svelte-h":!0}),s(d)!=="svelte-1jh7qpx"&&(d.textContent=xe),U=h(e,`.
+`),B=r(e,"P",{"data-svelte-h":!0}),s(B)!=="svelte-pntvnr"&&(B.innerHTML=Me),X=x(e),D=r(e,"P",{"data-svelte-h":!0}),s(D)!=="svelte-pntvnr"&&(D.innerHTML=_e),Y=x(e),M=r(e,"EMBED",{src:!0,width:!0,height:!0}),Z=x(e),F=r(e,"P",{"data-svelte-h":!0}),s(F)!=="svelte-pntvnr"&&(F.innerHTML=Ce),this.h()},h(){u(v,"href","https://metamask.io"),u(v,"target","_blank"),u(b,"href","https://brave.com"),u(b,"target","_blank"),u(f,"href","https://privacy-is-not-a-crime.de"),u(f,"target","_blank"),u(f,"data-svelte-h","svelte-mwxuuu"),u(d,"href","https://github.com/FreiheitsGeld/freiheitsgeld-paperwallet-generator"),u(d,"target","_blank"),Le(M.src,we="https://freiheitsgeld.github.io/freiheitsgeld-paperwallet-generator")||u(M,"src",we),u(M,"width","100%"),u(M,"height","810")},m(e,t){n(e,p,t),n(e,G,t),n(e,_,t),n(e,O,t),n(e,w,t),n(e,q,t),n(e,C,t),n(e,W,t),n(e,v,t),n(e,j,t),n(e,b,t),n(e,I,t),n(e,H,t),n(e,y,t),n(e,k,t),n(e,A,t),n(e,L,t),n(e,E,t),n(e,T,t),n(e,g,t),n(e,P,t),n(e,K,t),n(e,o,t),n(e,V,t),n(e,z,t),n(e,J,t),n(e,f,t),n(e,N,t),n(e,c,t),n(e,Q,t),n(e,S,t),n(e,R,t),n(e,d,t),n(e,U,t),n(e,B,t),n(e,X,t),n(e,D,t),n(e,Y,t),n(e,M,t),n(e,Z,t),n(e,F,t)},p:$,i:$,o:$,d(e){e&&(i(p),i(G),i(_),i(O),i(w),i(q),i(C),i(W),i(v),i(j),i(b),i(I),i(H),i(y),i(k),i(A),i(L),i(E),i(T),i(g),i(P),i(K),i(o),i(V),i(z),i(J),i(f),i(N),i(c),i(Q),i(S),i(R),i(d),i(U),i(B),i(X),i(D),i(Y),i(M),i(Z),i(F))}}}class Se extends Te{constructor(p){super(),Pe(this,p,null,oe,ke,{})}}export{Se as F};
